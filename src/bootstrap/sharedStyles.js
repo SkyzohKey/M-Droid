232ae59c8a3d22f92fe312d4034088f2cfecd6da
SkyzohKey <skyzohkey@protonmail.com>
2018-01-04 15:24:45 +0100

    Update Branding because people's pointed that white is too bright.

--- src/bootstrap/sharedStyles.js
+++ src/bootstrap/sharedStyles.js
@@ -1,12 +1,13 @@
 const sharedStyles = {
-  HEADER_COLOR: '#424242',
-  TAB_ACTIVE_COLOR: '#000',
-  TAB_INACTIVE_COLOR: '#BABABA',
+  HEADER_COLOR: '#e91e63',
+  HEADER_TEXT_COLOR: '#ffffff',
+  TAB_ACTIVE_COLOR: '#ffffff',
+  TAB_INACTIVE_COLOR: '#fdfdfd',
   ACCENT_COLOR: '#e91e63',
   RIPPLES_COLOR_DEFAULT: '#DADADA',
   screenContainer: {
     flex: 1,
-    backgroundColor: 'white'
+    backgroundColor: '#FAFAFA'
   }
 };
 
