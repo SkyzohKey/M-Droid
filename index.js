f621d68f072db9b06acbdc4f44946efcb7781e3b
SkyzohKey <skyzohkey@protonmail.com>
2018-01-06 01:52:08 +0100

    Woops, v0.5.0 version, stupid me. Not anymore in beta :)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 
-window.appVersion = 'v0.4.3-beta';
+window.appVersion = 'v0.5.0';
 
 // Instead of depending on yet another module, use the following for uuid v4 gen.
 window.uuidv4 = () => {
